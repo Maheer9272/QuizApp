c1ae29133d1a950eb7c943aaa2040f872999b761
Shaik Maheer <maheer9272@gmail.com>
2025-08-04 21:11:17 +0530

    2

--- src/main/java/com/maheer9272/quizapp/controller/QuestionController.java
+++ src/main/java/com/maheer9272/quizapp/controller/QuestionController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@RestController("/admin")
+@RestController()
 public class QuestionController {
 
     @Autowired
